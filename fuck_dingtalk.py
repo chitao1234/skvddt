ff002136a3dc584ed9b231c08e7580d8fc30031f
chitao1234 <1139954766@qq.com>
2020-04-03 10:44:44 +0800

    Add files via upload

--- fuck_dingtalk.py
+++ fuck_dingtalk.py
@@ -5,20 +5,33 @@ from time import sleep
 
 
 # 用户更改区域开始
+# 懒人模式,把json粘贴过来,自动解析视频ID,True是开,False是关
+LazyMode = True
+VideoInfo = \
+    {"source":3,"studyType":2,"resourceId":"103455343","packageId":"102605219","courseId":"102756065","courseTime":0,"learnTime":0,"type":1}
 # Time是次数,因为我这个代码每次刷增加5%,所以加了自动多刷几次的次数
-Times = 20
+Times = 2000
 # 这个是你在两次刷时间操作之间,停顿的秒数,建议不动,不然可能会被临时停止访问钉钉
 SleepSecond = 4
 # 下面三个在你换视频的时候要改,str()不要删,不要多加引号
-resourceId = str(103476085)
-packageId = str(102960314)
-courseId = str(101921004)
+if LazyMode:
+    # 开了懒人模式
+    resourceId = VideoInfo['resourceId']
+    packageId = VideoInfo['packageId']
+    courseId = VideoInfo['courseId']
+else:
+    # 没开懒人模式
+    resourceId = str(103582425)
+    packageId = str(102649199)
+    courseId = str(102197105)
 # 这两个暂时别动,是每次刷的时间相关的,还没搞清楚,不要多加引号
-courseTime = 50000
-learnTime = 6000
-# 这两个代表你是谁,在第一次要改,后面都不用改
-dd_sid = 'k0_25340b1b24e9855ec7fc_0b1b25345e85e9243c15420dba2f3d75e48511d9aa83'
-isg = 'BKCgK0e66mY9L1aPfZm-4tkDca5yqYRzIgXkgxq1HLtyFUc_wrpSAxvrqb-VpTxL'
+courseTime = 700
+learnTime = 600
+# 这两个代表你是谁,在每天第一次要改,后面都不用改
+# dd_sid=k0_1cc70ab1d787865ec84c_0ab11cc75e8687d753643b8b59ec567d4ec4d24784fd
+# isg=BMvLAQ1QERTORU0umsA183aKWm-1YN_iW0AihD3JPYpqXOi-xTH5MGs6Ml6y_Dfa
+dd_sid = 'k0_1cc70ab1d787865ec84c_0ab11cc75e8687d753643b8b59ec567d4ec4d24784fd'
+isg = 'BMvLAQ1QERTORU0umsA183aKWm-1YN_iW0AihD3JPYpqXOi-xTH5MGs6Ml6y_Dfa'
 # 用户更改区域结束
 
 Url = "https://saas.daxue.dingtalk.com/dingtalk/course/record.jhtml"
@@ -37,6 +50,7 @@ Cookies = {
     'up_ab': 'y',
     'preview_ab': 'y',
     'cna': 'LRLRFtSKfSICAXAxrxj3zKsn',
+    # 'XSRF-TOKEN': 'e0588ed4-443b-4c4b-9624-627e62e29861',
     'XSRF-TOKEN': '4a918264-173f-4049-baf7-1f5c2a58504b',
     'dd_sid': dd_sid,
     'isg': isg
@@ -72,10 +86,17 @@ for i in range(Times):
     print("Debug info stop.")
     print()
     print("Time:", i + 1)
-    if eval(Response.text.replace('\n', '').replace('\t', '').replace('true', 'True'))["data"] == 0:
+    ResponseFormatted = eval(Response.text.replace('\n', '').replace('\t', '').replace('true', 'True').replace('false', 'False'))
+    if ResponseFormatted['rgv587_flag'] == "sm":
+        # 若显示这个,请把SleepSecond调大,建议调回默认值:4
+        print("Failed: DoS Protection.")
+    if ResponseFormatted['success'] == 0:
+        print("Failed: General Fail:", ResponseFormatted['desc'])
+    if ResponseFormatted['data'] == 0:
         print("Failed: Video Not Found.")
     if Response.status_code == 200:
-        print("Success")
+        print("Success.")
     else:
         print("Failed:", Response.status_code)
     sleep(SleepSecond)
+print('Done.')
